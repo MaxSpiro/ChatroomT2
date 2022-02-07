0fe5a4a6e2e8918a5200e1fffa2119180b63547e
MaxSpiro <maxspiridonov04@gmail.com>
2022-02-07 11:56:41 -0800

    changed port

--- index.js
+++ index.js
@@ -36,12 +36,12 @@ io.on('connection', (socket) =>{
 
 
  // Use the server.listen(3000) when testing locally
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+// server.listen(3000, () => {
+//   console.log('listening on *:3000');
+// });
 
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 8000;
-// }
-// server.listen(port);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+server.listen(port);
