e2cebea5a7638d2e2c6afdedd9675576fe280d03
Roark Lacey <roarklacey@gmail.com>
2022-03-01 10:48:25 -0800

    Deleted Irrelevant JS Documents

--- JS/loginScript.js
+++
@@ -1,9 +0,0 @@
-// var socket = io();
-//
-// console.log("test");
-//
-// function login() {
-//   let username7 = document.getElementById("inputUsername").value;
-//   socket.emit('Username Typed', username7);
-//   location.href="../index.html";
-// }
