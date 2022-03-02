8a3922f7d47ad60c8d0e64a5afc10af65aa07e83
Roark Lacey <roarklacey@gmail.com>
2022-03-01 20:22:13 -0800

    JS Reformat + PORT

--- index.js
+++ index.js
@@ -102,13 +102,13 @@ function ExportIsTakenAccCheck( isTaken, accCheck, userIndex) {
 
 
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+// server.listen(3000, () => {
+//   console.log('listening on *:3000');
+// });
 
 
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 8000;
-// }
-// server.listen(port);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+server.listen(port);
